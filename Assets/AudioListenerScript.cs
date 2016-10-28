b60087d9cf546985683e07602384e3e7be7a62a8
Masaki Homma <masakihomma991201@gmail.com>
2016-10-28 21:02:59 +0900

    first commit

---
+++ Assets/AudioListenerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioListenerScript : MonoBehaviour {
+
+	public float speed = 100.0f;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		transform.position += new Vector3 (0,0,Time.deltaTime * speed);
+	}
+}
---
+++ Assets/AudioSourceScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AudioSourceScript : MonoBehaviour {
+
+	private AudioSource sound;
+	public AudioClip audioClip;
+	public int soundPitch = 440;
+	public float speed = 100.0f;
+
+	void Start () {
+		sound = gameObject.GetComponent <AudioSource> ();
+	}
+	
+	void Update () {
+		
+		transform.position += new Vector3 (Time.deltaTime * speed, 0, 0);
+
+		sound.clip = audioClip;
+		sound.pitch = soundPitch;
+		sound.Play ();
+	}
+}
